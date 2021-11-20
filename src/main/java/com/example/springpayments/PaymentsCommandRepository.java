78f5247fb4512cb6a75acb8526083f7254fb31d3
AnthonyNguyenT <anthonynguyenthanh@gmail.com>
2021-11-20 15:00:39 -0800

    implemented spring payments

---
+++ src/main/java/com/example/springpayments/PaymentsCommandRepository.java
@@ -0,0 +1,8 @@
+package com.example.springpayments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentsCommandRepository extends JpaRepository<PaymentsCommand, Long>
+{
+
+}
