a6bc316843f6a7752d9ad6a969b07cfe9031a4da
AnthonyNguyenT <anthonynguyenthanh@gmail.com>
2021-12-02 10:33:29 -0800

    CharityOrder and repo created, controller still needs to be coded

---
+++ src/main/java/com/example/endgame/CharityOrderRepository.java
@@ -0,0 +1,8 @@
+package com.example.endgame;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface CharityOrderRepository extends JpaRepository<CharityOrder, Long> 
+{
+    
+}
