29bb01dab3c1dd8362ac7f69476752be1ba168ee
abartolome-sjsu <68254762+abartolome-sjsu@users.noreply.github.com>
2021-12-05 23:22:47 -0800

    Modified users for login support

--- users/src/main/java/com/example/users/User.java
+++ users/src/main/java/com/example/users/User.java
@@ -13,7 +13,7 @@ import lombok.Data;
 
 @Data
 @Entity // Use 
-@Table(name = "accounts")
+@Table(name = "Users")
 public class User 
 {
     @Id
@@ -22,7 +22,7 @@ public class User
      
     @Column(nullable = false, unique = true, length = 45)
     private String email;
-     
+    
     @Column(nullable = false, length = 64)
     private String password;
      
